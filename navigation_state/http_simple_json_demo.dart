381437bf3e851ab9222941362d399ce1ddc66e8d
potash <35871896+potahyox@users.noreply.github.com>
2020-06-27 00:08:31 +0900

    Add files via upload

---
+++ navigation_state/http_simple_json_demo.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() async {
+  String url = 'http://jsonplaceholder.typicode.com/todos/1';
+  var response = await http.get(url);
+
+  Map<String, dynamic> data = jsonDecode(response.body);
+  print('userId: ${data['userId']}');
+  print('id: ${data['id']}');
+  print('title: ${data['title']}');
+  print('complete: ${data['completed']}');
+}
+
+
