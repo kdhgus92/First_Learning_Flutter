e5720b82ee0ccbec573d6fc38c7d1799dd6a142b
potash <35871896+potahyox@users.noreply.github.com>
2020-06-27 15:14:13 +0900

    Add files via upload

---
+++ hello_flutter/stateless_widget_demo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    title: 'Hello Flutter',
+    home: Scaffold(
+      appBar: AppBar(title: Text('Stateless 위젯 데모')),
+      body: _FirstStatelessWidget(),
+    )));
+
+class _FirstStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('이것은 stateless 위젯입니다.');
+  }
+}
